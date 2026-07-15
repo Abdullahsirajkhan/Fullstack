16f389494585edecde2b62f58cba77a182f07661
Abdullah Siraj Khan <abdullahsirajkhan2@gmail.com>
2026-07-15 15:00:32 +0500

    added practice

--- Frontend/codes/JS/11.js
+++ Frontend/codes/JS/11.js
@@ -1,3 +1,30 @@
-for(let i=0;i<=10;i++){
-    console.log(i);
+
+let title = document.querySelector("#main-title");
+title.textContent="changed!";
+
+console.log(title.textContent);
+
+let status = document.querySelector(".status-text");
+
+status.innerHTML = '<strong style="color: green;">Verified!</strong>';
+
+let link = document.querySelector("#user-link");
+
+console.log(link.getAttribute("href"));
+
+link.setAttribute("href", "https://google.com");
+
+link.removeAttribute("href");
+
+link.setAttribute("href" , "https://github.com");
+
+console.log(link.getAttribute("href"));
+
+let badge = document.querySelector("#badge");
+
+const lev = badge.getAttribute("data-level");
+
+if(lev=="guest"){
+    badge.setAttribute("data-level","admin");
+    badge.hidden = false;
 }
